740f34011c97c895cace3580b2f22d21b794b14d
jonathan.merino <jonathan.merino@ceiba.com.co>
2022-10-06 10:00:36 -0500

    Initial Commit

---
+++ NetflixClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixClone
+//
+//  Created by Jonathan Eduardo Meriño Bolívar - Ceiba Software on 6/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
